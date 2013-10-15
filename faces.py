00debc0ef6775cccb8685befa1cf8d0fcd85fde3
Matt Parrilla <matthew.parrilla@gmail.com>
2013-10-15 13:09:31 -0400

    Change font

--- faces.py
+++ faces.py
@@ -2,6 +2,9 @@ import json
 import glob
 import os
 
+male_answers = ['testA', 'testB', 'testC', 'testD']
+female_answers = ['test1', 'test2', 'test3', 'test4']
+
 
 def generate_json():
     face_list = glob.glob('static/img/faces/*.jpg')
@@ -10,22 +13,25 @@ def generate_json():
         json_dict = {}
         filename = os.path.split(face)[1]
         name = os.path.splitext(filename)[0][:-2]
-        sex = os.path.splitext(filename)[0][-1]
+        sex = os.path.splitext(filename)[0][-1].lower()
         json_dict['name'] = name.replace('_', ' ')
         json_dict['img'] = face
         json_dict['mp3'] = ('static/mp3/' + name + '.mp3')
         json_dict['sex'] = sex
-        json_dict['answers'] = ['TestA', 'TestB', 'TestC']
+        if sex == 'm':
+            json_dict['answers'] = male_answers
+        elif sex == 'f':
+            json_dict['answers'] = female_answers
+        else:
+            json_dict['answers'] = ['FAIL', 'FAIL', 'FAIL']
 
         if name == 'click_clack':
-            json_dict['name'] = 'Click &amp; Clack'
-            json_dict['answers'] = ['Bert &amp; Ernie', 'Harold &amp; Kumar',
-                'Simon &amp; Garfunkle']
+            json_dict['name'] = 'Click & Clack'
+            json_dict['answers'] = ['Bert & Ernie', 'Harold & Kumar',
+                'Simon & Garfunkle']
 
         json_list.append(json_dict)
     json_faces = json.dumps(json_list)
-    for i in json_list:
-        print i
     return json_faces
 
 generate_json()
--- static/js/faces.js
+++ static/js/faces.js
@@ -1,7 +1,19 @@
 $(document).ready( function() {
+    facesMadeForRadio = 0;
+
+    function shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }
+
     var data = JSON.parse($("#data").html());
 
-    $('button#play').on('click', function(e) {
+    $('#play_game').on('click', function(e) {
         var landingScreen = $(this).parents('#landing_screen');
         landingScreen.slideToggle();
     });
@@ -12,15 +24,33 @@ $(document).ready( function() {
 
     var questionList = $('div#questions').children();
 
-    for (i=0; i<data.length; i++) {
+    for (var i=0; i<data.length; i++) {
         var img= questionList.eq(i).children('img');
         var audio = questionList.eq(i).children('p.audio-p').children('audio');
         var buttonList = questionList.eq(i).children('.btn-group-vertical')
             .children('button');
+        var answerList = shuffleArray(data[i].answers).slice(0,2);
+        answerList.push(data[i].name);
+        var answers = shuffleArray(answerList);
+
+
+        for (var j=0; j<buttonList.length; j++) {
+            buttonList.eq(j).text(answers[j]);
+            if (answers[j] == data[i].name) {
+                var correctAnswer = buttonList.eq(j).attr('class');
+                buttonList.eq(j).attr('class', correctAnswer + ' correct');
+            }
+        }
+
 
         img.attr('src', data[i].img);
         audio.attr('src', data[i].mp3);
     }
+
+    $('button.correct').on('click', function(e) {
+        facesMadeForRadio += 10;
+        console.log(facesMadeForRadio);
+    });
 
 });
 
